d89b478dacd13e08f2eebe7c4acc875f248131d5
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2024-03-06 21:06:24 +0100

    remove cache fn

--- Makefile
+++ Makefile
@@ -13,7 +13,6 @@ deprovision_hana:
 
 deploy_app:
 	kubectl wait --for condition=established --timeout=60s crd/functions.serverless.kyma-project.io
-	(cd src/cache-fn && kyma apply function --dry-run -oyaml > ../../k8s-resources/fn-cache.yaml)
 	(cd src/hana-fn && kyma apply function --dry-run -oyaml > ../../k8s-resources/fn-hana.yaml)
 	cat k8s-resources/fn-template.yaml >> k8s-resources/fn-hana.yaml
 	kubectl apply -k k8s-resources
--- src/hana-fn/handler.js
+++ src/hana-fn/handler.js
@@ -3,7 +3,6 @@ const hana = require('@sap/hana-client');
 
 const { v4: uuidv4 } = require('uuid');
 
-
 var services = xsenv.getServices({
    hana: { name: 'hana' },
    hanaUrl: { name: 'hana-url' },
@@ -40,16 +39,39 @@ async function queryDB(sql) {
   }
   return results;
 }
+class Cache {
+  constructor() {
+    this.data = undefined;
+  }
+
+  async getData() {
+      if (!this.data){
+          try {
+            this.data = await queryDB(`SELECT * FROM BOOKS`) 
+          } catch (e) {
+            console.log(e)
+          }
+      }
+      return this.data
+  }
+
+  invalidate() {
+      this.data=undefined
+  }
+}
+
+const cache = new Cache();
 
 module.exports = {
     main: async function (event, context) {
         if (event.extensions.request.method === 'GET') {
-            const books = await queryDB(`SELECT * FROM BOOKS`);
+            const books = await cache.getData();
             return books
         } else if(event.extensions.request.method === 'POST'){
             let query = `insert into BOOKS values ('${uuidv4()}', '${event.extensions.request.body.title}', '${event.extensions.request.body.author}')`
             try {
                 let result =  await queryDB(query)
+                cache.invalidate();
                 return `${result} book added`
             } catch (err) {
                 return err.message;
