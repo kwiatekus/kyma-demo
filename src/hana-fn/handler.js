64fb1546447136aa286657e279b65d63b5fa3061
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2024-03-07 10:54:04 +0100

    add hana trace span

--- src/hana-fn/handler.js
+++ src/hana-fn/handler.js
@@ -14,28 +14,22 @@ services.hana.port = services.hanaUrl.port;
 
 const hanaConn = hana.createConnection();
 
+var tracer = undefined;
+
 async function queryDB(sql) {
+  const span = tracer.startSpan('query-hana');
+  span.setAttribute("sql", sql);
   try {
     await hanaConn.connect(services.hana);
-  } catch (err) {
-    console.error('queryDB connect', err.message, err.stack);
-    results = err.message;
-  }
-  try {
-    console.log(`schema: ${services.hana.schema}`)
     await hanaConn.exec('SET SCHEMA ' + services.hana.schema);
-    
     results = await hanaConn.exec(sql);
-    
   } catch (err) {
-    console.error('queryDB exec', err.message, err.stack);
+    console.error('queryDB ', err.message, err.stack);
     results = err.message;
   }
-  try {
-    await hanaConn.disconnect();
-  } catch (err) {
-    console.error('queryDB disconnect', err.message, err.stack);
-    results = err.message;
+  finally {
+    await hanaConn.disconnect()
+    span.end();
   }
   return results;
 }
@@ -64,6 +58,7 @@ const cache = new Cache();
 
 module.exports = {
     main: async function (event, context) {
+        tracer = event.tracer;
         if (event.extensions.request.method === 'GET') {
             const books = await cache.getData();
             return books
